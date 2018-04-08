8d1cdb6d0e9fc4284ebab704bfacdd1f38f60925
Hu Qiu <SHyH5@SHyH5deMacBook-Pro.local>
2018-04-08 14:21:29 +0800

    init pod
    create a project

---
+++ SHUxProject/SHUxProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SHUxProject
+//
+//  Created by SHyH5 on 2018/4/8.
+//  Copyright © 2018年 SHyH5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
