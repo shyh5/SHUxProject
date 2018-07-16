99faf60a0897ec169bc65b1b89326b8f61c52e34
SHyH5 <1475966605@qq.com>
2018-07-16 17:48:31 +0800

    上传一些面试题总结
    oc 和 swift的都有

---
+++ SwiftInterviewDemo/SwiftInterviewDemo/Class/多线程/SHGCDViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SHGCDViewController.swift
+//  SwiftInterviewDemo
+//
+//  Created by gg on 2018/7/16.
+//  Copyright © 2018年 gg. All rights reserved.
+//
+
+import UIKit
+
+class SHGCDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "swift 多线程"
+        view.backgroundColor = .white
+        
+        initData()
+    }
+    
+    func initData() {
+        
+    }
+
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
